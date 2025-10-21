bcf0e157884729de92c60d64bc79f03ce4d8dee4
C Fox <235750736+CharlieFox84@users.noreply.github.com>
2025-10-20 21:22:12 -0700

    Infra: Refine commit log script with Markdown formatting and newline compliance

--- .devtools/update-commit-log.sh
+++ .devtools/update-commit-log.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# *** previous version ***
+# Generate a markdown changelog of commits from the last 30 days
+# echo -e "# Change Log (Last 30 Days)\n\nUpdated: $(date)\n" > commit.md
+# git log dev --since="30 days ago" --pretty=format:"- %s (%h)" >> commit.md
+# *** end previous version ***
+
 # Generate a markdown changelog of commits from the last 30 days
 echo -e "# Change Log (Last 30 Days)\n\nUpdated: $(date)\n" > commit.md
-git log dev --since="30 days ago" --pretty=format:"- %s (%h)" >> commit.md
+
+git log dev --since="30 days ago" --pretty=format:"- %s (%h)" |
+  sed -E 's|https://github.com/([^/]+/[^ )]+)|**\1**|g' >> commit.md
+
+# Ensure file ends with a newline
+echo "" >> commit.md
